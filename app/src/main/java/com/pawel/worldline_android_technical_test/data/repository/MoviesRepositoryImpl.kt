ff2d19ea2545a2bffc802456fb19a8de8720fc50
Grzesik <pawel.grzesik@worldline.com>
2022-02-18 11:58:21 +0100

    Move the cache in memory management to Repository

--- app/src/main/java/com/pawel/worldline_android_technical_test/data/repository/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/pawel/worldline_android_technical_test/data/repository/MoviesRepositoryImpl.kt
@@ -1,17 +1,28 @@
 package com.pawel.worldline_android_technical_test.data.repository
 
+import com.pawel.worldline_android_technical_test.cache.MoviesInMemoryCache
 import com.pawel.worldline_android_technical_test.data.api.MoviesRepositoryNetwork
 import com.pawel.worldline_android_technical_test.data.model.movie.Movie
 import com.pawel.worldline_android_technical_test.data.model.movies.MovieApiResponse
+import com.pawel.worldline_android_technical_test.data.model.movies.Result
+import com.pawel.worldline_android_technical_test.util.Consts.MOVIES_IN_MEMORY_KEY
 import javax.inject.Inject
 
 class MoviesRepositoryImpl @Inject constructor(private val moviesRepositoryNetwork: MoviesRepositoryNetwork) :
     MoviesRepository {
-    override suspend fun getMovies(): MovieApiResponse {
-        return moviesRepositoryNetwork.getMovies()
-    }
 
-    override suspend fun getMovie(movieID : String): Movie {
-        return moviesRepositoryNetwork.getMovieDetail(movieID)
-    }
+    @Inject
+    lateinit var cache: MoviesInMemoryCache
+
+    override fun getMoviesFromCache(): List<Result>? =
+        cache.fetchFromCacheMemory(MOVIES_IN_MEMORY_KEY)
+
+    override fun saveMoviesToCache(listResult: List<Result>) =
+        cache.saveInCacheMemory(MOVIES_IN_MEMORY_KEY, listResult)
+
+    override suspend fun getMovies(): MovieApiResponse =
+        moviesRepositoryNetwork.getMovies()
+
+    override suspend fun getMovie(movieID: String): Movie =
+        moviesRepositoryNetwork.getMovieDetail(movieID)
 }
--- app/src/main/java/com/pawel/worldline_android_technical_test/service/MoviesServiceImpl.kt
+++ app/src/main/java/com/pawel/worldline_android_technical_test/service/MoviesServiceImpl.kt
@@ -1,6 +1,5 @@
 package com.pawel.worldline_android_technical_test.service
 
-import com.pawel.worldline_android_technical_test.cache.MoviesInMemoryCache
 import com.pawel.worldline_android_technical_test.data.repository.MoviesRepository
 import com.pawel.worldline_android_technical_test.data.model.movies.Result
 import javax.inject.Inject
@@ -9,23 +8,14 @@ import javax.inject.Inject
 class MoviesServiceImpl @Inject constructor(private val moviesRepository: MoviesRepository) :
     MoviesService {
 
-    companion object {
-        const val MOVIES_IN_MEMORY_KEY = "movies_in_memory_key"
-    }
+    override suspend fun getMovies(): List<Result>? = moviesRepository.getMoviesFromCache() ?: getMoviesFromNetwork()
 
-    @Inject lateinit var cache: MoviesInMemoryCache
-    private var existing: List<Result>? = null
-
-    override suspend fun getMovies(): List<Result>? {
-        existing = cache.fetchFromCacheMemory(MOVIES_IN_MEMORY_KEY)
-        val listResult: List<Result>?
-        if (existing.isNullOrEmpty()) {
-            listResult = moviesRepository.getMovies().results
-            cache.saveInCacheMemory(MOVIES_IN_MEMORY_KEY, listResult)
-        } else {
-            listResult = existing as List<Result>
+    private suspend fun getMoviesFromNetwork(): List<Result>? {
+        val movies = moviesRepository.getMovies().results
+        movies?.let {
+            moviesRepository.saveMoviesToCache(movies)
         }
-        return listResult
+        return movies
     }
 
     override suspend fun getMovie(movieID: String) = moviesRepository.getMovie(movieID)
