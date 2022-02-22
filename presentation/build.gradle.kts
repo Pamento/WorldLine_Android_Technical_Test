7635aa77e5292759e047cc2d522a10a2ba16c764
Grzesik <pawel.grzesik@worldline.com>
2022-02-22 15:24:16 +0100

    Rename, refactor of build.gradle of presentation module

---
+++ presentation/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("module-plugin")
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation(project(":domain"))
+
+    implementation(Dependencies.ANDROIDX_CORE_KTX)
+    implementation(Dependencies.ANDROIDX_APPCOMPAT)
+    implementation(Dependencies.ANDROID_MATERIAL)
+    implementation(Dependencies.HILT)
+    kapt(Dependencies.HILT_KAPT)
+    testImplementation(Dependencies.JUNIT4)
+    androidTestImplementation(Dependencies.TEST_EXT)
+    androidTestImplementation(Dependencies.ESPRESSO)
+}
