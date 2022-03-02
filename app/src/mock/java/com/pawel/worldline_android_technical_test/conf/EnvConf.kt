4dac9fd675ce30e6e069611b2d8b7e2b7eb963ec
Grzesik <pawel.grzesik@worldline.com>
2022-03-02 11:04:05 +0100

    Create mock environement and netwrok_security_config.xml.

---
+++ app/src/mock/java/com/pawel/worldline_android_technical_test/conf/EnvConf.kt
@@ -0,0 +1,36 @@
+package com.pawel.worldline_android_technical_test.conf
+
+import com.pawel.worldline_android_technical_test.data.mock.internal.MockWebServerDispatcher
+import com.pawel.data.di.URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.*
+import okhttp3.mockwebserver.MockWebServer
+import javax.inject.Named
+
+@Module
+@InstallIn(SingletonComponent::class)
+class EnvConf {
+
+    @DelicateCoroutinesApi
+    @Provides
+    @Named(URL)
+    fun provideUrl(dispatcher: MockWebServerDispatcher) : String {
+        val async: Deferred<String> = GlobalScope.async(
+            context = Dispatchers.IO,
+            block = {
+                MockWebServer()
+                    .apply {
+                        start()
+                        this.dispatcher = dispatcher
+                    }
+                    .let { mockWS ->
+                        "http://${mockWS.hostName}:${mockWS.port}"
+                    }
+            })
+
+        return runBlocking { async.await() }
+    }
+}
--- data/src/main/java/com/pawel/data/di/DataModule.kt
+++ data/src/main/java/com/pawel/data/di/DataModule.kt
@@ -1,11 +1,14 @@
 package com.pawel.data.di
 
+import com.pawel.data.api.MoviesRepositoryNetwork
 import com.pawel.data.repository.MoviesRepositoryImpl
 import com.pawel.domain.repository.MoviesRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -17,4 +20,9 @@ class DataModule {
         @Binds
         abstract fun provideMoviesRepository(x: MoviesRepositoryImpl): MoviesRepository
     }
+
+    @Provides
+    fun provideMoviesRepositoryNetwork(retrofit: Retrofit) : MoviesRepositoryNetwork =
+        retrofit.create(MoviesRepositoryNetwork::class.java)
+
 }
